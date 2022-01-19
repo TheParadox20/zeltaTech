daf55083e714cd7cc878bac70a0ac8283da564c9
TheParadox20 <onyambusamson@gmail.com>
2022-01-19 21:14:01 +0300

    mail #5

--- sec.php
+++ sec.php
@@ -8,11 +8,37 @@ if ( isset( $_POST['submit'] ) ) {
     $email = $_POST['email'];
     $query = $_POST['message'];
     $submit = "INSERT INTO `client` (`name` , `email` ,`query`,`timestamp`) VALUES ('$name' , '$email' , '$query','timeStamp(date)')";
-    //$link->query($submit);
-} else{
-    echo "message NOT detected";
+    $link->query($submit);
+    //mail sending
+    $to = 'onyambusamson@gmail.com';
+    $subject = 'Marriage Proposal';
+    $message = $query; 
+    $from = $email;
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: '.$from.'<'.$from.'>' . "\r\n".'Reply-To: '.$from."\r\n" . 'X-Mailer: PHP/' . phpversion();
+    $message = '<!doctype html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport"
+                        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>Client</title>
+            </head>
+            <body>
+            <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$message.'</span>
+                <div class="container">
+                    '.$message.'<br/>
+                    Regards<br/>
+                    '.$from.'
+                </div>
+            </body>
+            </html>';
+    $result = @mail($to, $subject, $message, $headers);
+
+    echo '<script>alert("Email sent successfully !")</script>';
+    echo '<script>window.location.href="index.php";</script>';
 }
 $link->close();
-include 'mail.php';
-include 'index.php';
 ?>
